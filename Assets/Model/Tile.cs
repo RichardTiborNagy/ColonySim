52d57cf61daa152de6d0fd847db42c587f9067a8
Richard Tibor Nagy <richard.tibor.nagy@gmail.com>
2018-03-01 21:48:11 +0100

    Tiles can now get an array of their neighbors

--- Assets/Model/Tile.cs
+++ Assets/Model/Tile.cs
@@ -19,6 +19,19 @@ public class Tile : IDisplayable
     public int Y { get; }
     //public TileType Type { get; private set; }
 
+    public Tile[] Neighbors
+    {
+        get
+        {
+            Tile[] neighbors = new Tile[4];
+            neighbors[0] = World.Current[X + 1, Y];
+            neighbors[1] = World.Current[X, Y + 1];
+            neighbors[2] = World.Current[X - 1, Y];
+            neighbors[3] = World.Current[X, Y - 1];
+            return neighbors;
+        }
+    }
+
     public event Action Changed;
 
     public void OnChange()
