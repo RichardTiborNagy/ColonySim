e38bc79a593ed8cf37c76365edff243253797d6b
Richard Tibor Nagy <richard.tibor.nagy@gmail.com>
2018-03-02 13:00:45 +0100

    Camera panning

---
+++ Assets/View/UI/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float PanningSpeed = 20f;
+    public int PanningBorder = 10;
+
+    private void Update ()
+	{
+	    var position = transform.position;
+
+	    if (Input.mousePosition.x >= Screen.width - PanningBorder)
+	        position.x += PanningSpeed * Time.deltaTime;
+	    if (Input.mousePosition.x <= PanningBorder)
+	        position.x -= PanningSpeed * Time.deltaTime;
+        if (Input.mousePosition.y >= Screen.height - PanningBorder)
+	        position.y += PanningSpeed * Time.deltaTime;
+	    if (Input.mousePosition.y <= PanningBorder)
+	        position.y -= PanningSpeed * Time.deltaTime;
+
+	    transform.position = position;
+	}
+}
