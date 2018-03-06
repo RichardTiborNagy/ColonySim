d3523884ab2ee9cf0027e3dfa8cc5e1a17d4cc66
Richard Tibor Nagy <richard.tibor.nagy@gmail.com>
2018-03-06 21:58:30 +0100

    Started working on the robots

---
+++ Assets/Model/Robot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Robot : IDisplayable
+{
+    public RobotType Type { get; private set; }
+
+    public Tile Tile { get; private set; }
+
+    private Tile NexTile { get; set; }
+
+    public float MovementProgress { get; private set; }
+
+    public int Speed { get; private set; }
+
+    public Job Job { get; private set; }
+
+    public State State { get; private set; }
+
+    public Robot(RobotType type, int speed)
+    {
+        Type = type;
+        Speed = speed;
+        MovementProgress = 0f;
+    }
+    
+    #region IDisplayable interface implementation
+    public int X => Tile.X;
+    public int Y => Tile.Y;
+
+    public event Action Changed;
+    public void OnChange()
+    {
+        Changed?.Invoke();
+    }
+    #endregion
+}
