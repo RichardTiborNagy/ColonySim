21066e14c2ed53234fb058ce6bed4008a3da2cc9
Richard Tibor Nagy <richard.tibor.nagy@gmail.com>
2018-03-10 18:27:53 +0100

    Jobs have progress now

--- Assets/Model/Job.cs
+++ Assets/Model/Job.cs
@@ -7,19 +7,30 @@ public class Job : IPrototypable
 
     public Tile Tile { get; set; }
 
+    public float TimeToComplete { get; private set; }
+
     public float Progress { get; set; }
+
+    public void Work(float deltaTime)
+    {
+        Progress += deltaTime;
+    }
+
+    public bool IsComplete => Progress >= TimeToComplete;
 
     private Action<Job> OnComplete { get; set; }
 
-    public Job(string type, Action<Job> onComplete)
+    public Job(string type, Action<Job> onComplete, float timeToComplete)
     {
         Type = type;
         OnComplete = onComplete;
+        TimeToComplete = timeToComplete;
         Progress = 0f;
     }
 
     public Job(Job other)
     {
+        TimeToComplete = other.TimeToComplete;
         Type = other.Type;
         OnComplete = other.OnComplete;
     }
