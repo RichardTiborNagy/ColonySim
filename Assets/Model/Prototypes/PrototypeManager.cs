712cd8ee914e8cde37f9cf63aeded994ceb50ca2
Richard Tibor Nagy <richard.tibor.nagy@gmail.com>
2018-03-10 18:28:29 +0100

    Moved prototype related files to a new folder

---
+++ Assets/Model/Prototypes/PrototypeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrototypeManager<T> where T : IPrototypable
+{
+    private Dictionary<string, T> _prototypes;
+
+    public T Get(string type)
+    {
+        return _prototypes[type];
+    }
+
+    public void Add(T proto)
+    {
+        _prototypes.Add(proto.Type, proto);
+    }
+
+    public PrototypeManager()
+    {
+        _prototypes = new Dictionary<string, T>();
+    }
+}
--- Assets/Model/RobotType.cs
+++
@@ -1,6 +0,0 @@
-﻿public enum RobotType
-{
-    Gathering,
-    Construction,
-    Guard
-}
